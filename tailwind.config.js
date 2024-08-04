267b2d254dcbccc255c7e4680b1a60174fa3a756
shorif <sitarek77@gmail.com>
2024-08-04 16:56:14 +0600

    4.56

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html"],
+  content: ["./src/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'navyblue': '#233A5F'
+      }
+    },
   },
   plugins: [],
 }
 
+// npx tailwindcss -i ./src/style.css -o ./src/output.css --watch
